792b1ed53f60c597de8dc44606a00e0654abf21f
Vivek Raj <VivekRaj27082005@gmail.com>
2024-11-09 11:08:13 +0530

    Boiler Plate

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC04FVQD7iB_RzohTNpaslojLcVSd4gfD4",
+  authDomain: "odoo-hackathon-30ef1.firebaseapp.com",
+  projectId: "odoo-hackathon-30ef1",
+  storageBucket: "odoo-hackathon-30ef1.firebasestorage.app",
+  messagingSenderId: "38426591989",
+  appId: "1:38426591989:web:3f9cbe9ff7703521413bbc",
+  measurementId: "G-LMLXN396J0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default { app };
